25f45e7f9e73e3264ef801abe063f1ef8deea07c
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2024-05-29 16:36:36 -0300

    Configuração de rotas

--- public/index.php
+++ public/index.php
@@ -20,24 +20,20 @@ $pdo = new PDO(dsn:'mysql:host=localhost;dbname=aluraplay', username:'root', pas
 
 $videoRepository = new VideoRepository($pdo);
 
-if (!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/') {
-    $controller = new VideoListController($videoRepository);
-} elseif ($_SERVER['PATH_INFO'] === '/novo-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $controller = new VideoFormController($videoRepository);
-        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $controller = new NewVideoController($videoRepository);
-        }
-} elseif ($_SERVER['PATH_INFO'] === '/editar-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-            $controller = new VideoFormController($videoRepository);
-        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $controller = new EditVideoController($videoRepository);
-    }
-} elseif ($_SERVER['PATH_INFO'] === '/remover-video') {
-        $controller = new DeleteVideoController($videoRepository);
+$routes = require_once __DIR__ . '/../config/routes.php'; // Definindo as rotas
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+$key = "$httpMethod|$pathInfo";
+
+if(array_key_exists($key,$routes)) {
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+
+    $controller = new $controllerClass($videoRepository);
 } else {
-        $controller = new Error404Controller();
+    $controller = new Error404Controller();
 }
+
 /** @var \Alura\Mvc\Controller\Controller $controller */
 $controller->processaRequisicao();
